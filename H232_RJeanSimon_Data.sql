e3abd25583f41c7fdfb4ab4e9db5c258004d8769
JeanSimon <53907520+JeanSimon20@users.noreply.github.com>
2023-12-13 12:18:55 -0500

    Update H232_RJeanSimon_Data.sql

--- H232_RJeanSimon_Data.sql
+++ H232_RJeanSimon_Data.sql
@@ -10,15 +10,15 @@ INSERT INTO area (name) VALUES ('Recursos Humanos');
 INSERT INTO area (name) VALUES ('Ventas');
 
 -- Insertar 5 registros en la tabla Maestra, Persona
-SET DATEFORMAT dmy;
-INSERT INTO PERSON (name, dni, email, gender, fecha_cumpleanos, celular) VALUES ('Jean Simon', '76093168', 'jeansimon@gmail.com','M','24/03/2001','924584301');
+SET DATEFORMAT dmy
+INSERT INTO PERSON (name, dni, email, gender, fecha_cumpleanos, celular) VALUES ('Jean Simon', '76093168', 'jeansimon@gmail.com','M','10/05/2023','924584301');
 INSERT INTO PERSON (name, dni, email, gender, fecha_cumpleanos, celular) VALUES ('Claudio Fernandez', '76093169', 'claudio@gmail.com','M','25/03/2001','924584302');
 INSERT INTO PERSON (name, dni, email, gender, fecha_cumpleanos, celular) VALUES ('Mateo Arcos', '76090019', 'mateo@outlook.com','M','25/12/2001','924584306');
 INSERT INTO PERSON (name, dni, email, gender, fecha_cumpleanos, celular) VALUES ('Jesus Gutierrez', '76093219', 'jgutierrez@hotmail.com','M','02/11/2001','924584631');
 INSERT INTO PERSON (name, dni, email, gender, fecha_cumpleanos, celular) VALUES ('Maria Eulogio', '76193219', 'mgutierrez@hotmail.com','D','25/05/2001','924584633');
 
 -- Insertar 5 registros en la tabla Maestra, Bienes
-SET DATEFORMAT dmy;
+SET DATEFORMAT dmy
 INSERT INTO BIENES (cantidad, code, detalle, valorlibro, fecha_ingreso, fecha_depreciacion, depreciacion_anual, depreciacion_mensual, depreciacion_acumulada, status) VALUES ('1','EQ-001', 'Escritorio color Gris', '800 c/u','1/02/2023','31/12/2023','0.80','0.007','0.80','Baja');
 INSERT INTO BIENES (cantidad, code, detalle, valorlibro, fecha_ingreso, fecha_depreciacion, depreciacion_anual, depreciacion_mensual, depreciacion_acumulada, status) VALUES ('1','EQ-002', 'Escritorio color Gris', '800 c/u','1/02/2023','31/12/2023','0.80','0.007','0.80','Baja');
 INSERT INTO BIENES (cantidad, code, detalle, valorlibro, fecha_ingreso, fecha_depreciacion, depreciacion_anual, depreciacion_mensual, depreciacion_acumulada, status) VALUES ('1','EQ-003', 'Escritorio color Gris', '800 c/u','1/02/2023','31/12/2023','0.80','0.007','0.80','Baja');
